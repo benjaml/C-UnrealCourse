80146722b7e9296057e3627b54d8d4e5f3696f1b
benjaml <b.lefevre@rubika-edu.com>
2016-03-31 11:22:25 +0200

    first push of basic content

---
+++ Source/CodeCourse/CodeCourseGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CodeCourseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODECOURSE_API ACodeCourseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
