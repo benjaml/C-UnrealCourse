9789a24cac4658b414d6c7a5ba1e3dde4d47a929
benjaml <b.lefevre@rubika-edu.com>
2016-04-02 16:40:07 +0200

    Modification on the map and add of OpenDoor C++ component

--- Source/CodeCourse/OpenDoor.cpp
+++ Source/CodeCourse/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "CodeCourse.h"
-#include "PositionReporter.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -17,20 +17,17 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	FString objectName = GetOwner()->GetName();
-	FString objectPos = "X=Something, Y=Something, Z=Something";
-	FVector position = GetOwner()->GetActorLocation();
-	objectPos = position.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *objectName, *objectPos);
+
+	GetOwner()->SetActorRotation(FRotator(0, 50, 0));
 	
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
